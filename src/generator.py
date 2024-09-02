6b008f1d06f64fe29c617aa6b640629d0fe5d7da
HubertM6 <119881354+HubertM6@users.noreply.github.com>
2024-09-02 23:10:22 +0200

    Removing what is redundant

--- src/generator.py
+++
@@ -1,7 +0,0 @@
-from random import randrange
-
-if __name__ == '__main__':
-    with open('big_test.txt', "w") as file:
-        for i in range(1000000):
-            number = randrange(1, pow(2, 14))
-            file.write(bin(number)[2:] + '\n')
